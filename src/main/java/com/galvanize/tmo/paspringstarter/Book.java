2d62129da3527495704a1c2546f23aacaf9acd8a
Avinash Akula <avinash.akula1@t-mobile.com>
2022-05-17 22:17:41 -0500

    remove imports

--- src/main/java/com/galvanize/tmo/paspringstarter/Book.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -1,8 +1,5 @@
 package com.learning.restfulwebservices.book;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 public class Book {
 	
